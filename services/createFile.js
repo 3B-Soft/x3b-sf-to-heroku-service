db854043a13ce7cda3bf260879b15de40884b7b3
James Ridge <james@3bonboarding.com>
2025-11-27 17:11:23 +0200

    Share files

--- services/createFile.js
+++ services/createFile.js
@@ -2,7 +2,7 @@ import { handleFetch } from '../utils/fetch.js';
 
 const API_VER = 'v62.0';
 
-export default async function ({ record, endpoint, sessionId }) {
+export default async function ({ record, endpoint, sessionId, namespace = 'b3f' }) {
     const url = `${endpoint}/services/data/${API_VER}/sobjects/ContentVersion`;
 
     const response = await handleFetch(url, {
@@ -13,6 +13,27 @@ export default async function ({ record, endpoint, sessionId }) {
         },
         body: JSON.stringify(record),
     });
+
+    console.log('Create file response', response);
+    if (response.success && record.FirstPublishLocationId) {
+        const shareResponse = await handleFetch(`${endpoint}/services/apexrest/${namespace}/GlobalRemotingRouter/`, {
+            method: 'POST',
+            headers: {
+                Authorization: `Bearer ${sessionId}`,
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                controllerName: `${namespace}.Shared`,
+                endp: 'shareFile',
+                contentVersionId: response.id,
+                linkedEntityId: record.FirstPublishLocationId
+            }),
+        }).then(res => JSON.parse(res));
+        console.log('Shhare File Response', shareResponse)
+        if (!shareResponse.success) {
+            console.error('Failed to share file', shareResponse);
+        }
+    }
 
     return response;
 }
