6edbb6cde33d53e8f09fdabe31c255e8dadc7809
James Ridge <james@3bonboarding.com>
2025-11-29 12:21:05 +0200

    add health check

--- index.js
+++ index.js
@@ -25,7 +25,12 @@ app.use(express.json({ limit: "50mb" }));
 app.use(express.static(__dirname));
 
 
-
+app.route('/health').get(async function (req, res) {
+    return res.status(200).json({
+        success: true,
+        responseObject: null
+    });
+});
 /**
  * Get a Salesforce file using contentVersion
  */
